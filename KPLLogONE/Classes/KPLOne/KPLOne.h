c1aebf7999d22b81ac624cd46aec821bc18c33e9
MrXiaoYang <582385082@qq.com>
2017-06-15 11:36:27 +0800

    s

---
+++ KPLLogONE/Classes/KPLOne/KPLOne.h
@@ -0,0 +1,15 @@
+//
+//  KPLOne.h
+//  Pods
+//
+//  Created by apple on 17/6/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KPLOne : NSObject
+
++ (void)kpl_one;
+
+@end
---
+++ KPLLogONE/Classes/KPLOne/KPLOne.m
@@ -0,0 +1,18 @@
+//
+//  KPLOne.m
+//  Pods
+//
+//  Created by apple on 17/6/15.
+//
+//
+
+#import "KPLOne.h"
+
+@implementation KPLOne
+
++ (void)kpl_one
+{
+    NSLog(@"one");
+}
+
+@end
